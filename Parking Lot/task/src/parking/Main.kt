ecc4d193c6f3ed28ff64613c503d5e526ada5fb9
acestus <acestus@debian-BULLSEYE-live-builder-AMD64>
2022-01-07 15:40:03 -0500

    Initial commit

---
+++ Parking Lot/task/src/parking/Main.kt
@@ -0,0 +1,9 @@
+package parking
+
+fun main() {
+    println("White car has parked.")
+    println("Yellow car left the parking lot.")
+    println("Green car just parked here.")
+    println(20/ 3 + 4 * 3)
+
+}
